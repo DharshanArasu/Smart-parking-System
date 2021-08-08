58beb855d62cee732c19df5f158c7d2e634a0b5f
dharshanprofs@gmail.com <dharshandeez111@gmail.com>
2021-08-08 22:36:01 +0530

    hello

--- src/components/TenTodos.js
+++ src/components/TenTodos.js
@@ -7,22 +7,11 @@ function TenTodos() {
 	let dispatch=useDispatch();
   return (
 <div>
-<br/>
-<br/>
-<br/>
-<br/>
+
 <hr/>
 <div className="text-center">
-<h1>You will get 10 Spaces at a time</h1>
-				<button 
-					onClick={()=>
-					{
-					
-						
-						dispatch(extendTodo(
-						));
-					}}
-				className="btn btn-primary  mx-2"><strong>CLICK HERE FOR NEW SPACES</strong></button>
+
+				
 				
 				<hr/></div></div>
   );
